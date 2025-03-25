0ee36d1a4a6d05358b5b11b73e6ef6831e5375be
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-03-25 23:11:58 +1000

    Added simple health check.

--- app_cough/views/routes.py
+++ app_cough/views/routes.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.get('/health')
+def get_health():
+    #health logic will come when db and other services are added
+    return {"status": "ok"}
