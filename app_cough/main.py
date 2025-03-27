77c5cf7cac84e1f73acf69756ef6a158684a03c6
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-03-27 20:02:09 +1000

    missed this file

--- app_cough/main.py
+++ app_cough/main.py
@@ -1,6 +1,18 @@
+import os
+import psycopg2
 from fastapi import FastAPI
-from app_cough import healthrouter
+from app_cough import healthrouter, labrouter
+from .models import engine, seed_labs, Base, dbmodels
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    dbmodels.Base.metadata.create_all(bind=engine)
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # Directory of main.py
+    file_path = os.path.join(base_dir, "labs.csv")  # Correct file path
+    #seed_labs(file_path)
+
 app.include_router(healthrouter, prefix="/api/v1")
+app.include_router(labrouter, prefix="/api/v1")
