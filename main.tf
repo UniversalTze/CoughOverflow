b78711e6b34ff8d461ec3c9b616b043f668933bd
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-04-10 19:43:37 +1000

    Just some tests.

--- main.tf
+++ main.tf
@@ -11,15 +11,62 @@ provider "aws" {
     region = "us-east-1"
     shared_credentials_files = ["./credentials"]
     default_tags {
-        tags = {
+        tags = { # For metadata, # Key value pairs. (all resources created will inherit these tags, unless overrided).
             Course       = "CSSE6400"
             Name         = "CoughOverflow"
             Automation   = "Terraform"
+            Owner        = "UniversalTze"
+            StudentID    = "s4703754"
         }
     }
 }
+/*
+# Resource
+locals {
+    image = "ghcr.io/csse6400/taskoverflow:latest"  # Currently incorrect, need to change this with image in registry
+    database_username = "administrator" 
+    database_password = "foobarbaz" # This is bad! 
+} 
+
+resource "aws_ecr_repository" "coughOverflow" { #ECR Registry
+ name = "coughoverflow" 
+}
+
+resource "aws_security_group" "coughOverflow_database" { 
+ name = "coughoverflow_database"  # Name of security group in AWS
+ description = "Set up inbound and outbound Postgresql traffic" 
+ 
+ ingress {  #Inbound (TCP port 5432) default for postgress
+   from_port = 5432 
+   to_port = 5432 
+   protocol = "tcp" 
+   cidr_blocks = ["0.0.0.0/0"] #Any IPv4 address
+ } 
+ 
+ egress { #outbound (all outbound traffic)
+   from_port = 0 
+   to_port = 0 
+   protocol = "-1" #represents all
+   cidr_blocks = ["0.0.0.0/0"]  #IPv4
+   ipv6_cidr_blocks = ["::/0"]  #IPv6
+ } 
+ 
+ tags = { 
+   Name = "coughoverflow_database"   #metadata (AWS generic tagging system)
+   # Commonly used for display in AWS consoles
+ } 
+}
+
+resource "docker_image" "coughOverflow" { 
+ name = "${aws_ecr_repository.coughoverflow}:latest" 
+ build { 
+   context = "." #build image locally
+ } 
+} 
+
 
 resource "local_file" "url" {
     content  = "http://my-url/"  # Replace this string with a URL from your Terraform.
     filename = "./api.txt"
 }
+*/
