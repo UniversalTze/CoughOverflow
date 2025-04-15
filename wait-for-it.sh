d4791929e7c8762b6dc4aa9d4dc162993276a1ff
Mr Goh <138341937+UniversalTze@users.noreply.github.com>
2025-04-16 08:28:24 +1000

    Update wait-for-it.sh
    testv2

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-HEALTH_URL="http://localhost:8080/api/v1/health"
-echo "Health URL: $HEALTH_URL"
-echo "Waiting for service to be healthy"
-until curl -s -o /dev/null -w "%{http_code}" "$HEALTH_URL" | grep -q "200"; do
-  echo -n "."
-  sleep 1
-done
-echo "service healthy"
+#HEALTH_URL="http://localhost:8080/api/v1/health"
+#echo "Health URL: $HEALTH_URL"
+#echo "Waiting for service to be healthy"
+#until curl -s -o /dev/null -w "%{http_code}" "$HEALTH_URL" | grep -q "200"; do
+#  echo -n "."
+#  sleep 1
+#done
+#echo "service healthy"
