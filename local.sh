67e3f6a2925c002b5101cc77467f0388e4057ee9
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-04-16 03:13:16 +1000

    test

--- local.sh
+++ local.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker compose up --build -d
-HEALTH_URL="http://localhost:8080/api/v1/health"
-echo "Health URL: $HEALTH_URL"
-echo "Waiting for service to be healthy"
-until curl -s -o /dev/null -w "%{http_code}" "$HEALTH_URL" | grep -q "200"; do
-  echo -n "."
-  sleep 1
-done
-echo "service healthy"
+#HEALTH_URL="http://localhost:8080/api/v1/health"
+#echo "Health URL: $HEALTH_URL"
+#echo "Waiting for service to be healthy"
+#until curl -s -o /dev/null -w "%{http_code}" "$HEALTH_URL" | grep -q "200"; do
+#  echo -n "."
+#  sleep 1
+#done
+#echo "service healthy"
---
+++ wait-for-it.sh
@@ -0,0 +1,8 @@
+HEALTH_URL="http://localhost:8080/api/v1/health"
+echo "Health URL: $HEALTH_URL"
+echo "Waiting for service to be healthy"
+until curl -s -o /dev/null -w "%{http_code}" "$HEALTH_URL" | grep -q "200"; do
+  echo -n "."
+  sleep 1
+done
+echo "service healthy"
