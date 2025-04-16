800331b5b56cd13469d53756a384a6028fde473d
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-04-16 11:57:30 +1000

    Added a curl request for api service

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export DOCKER_BUILDKIT=1
 terraform init
 
 terraform apply -auto-approve
+
+until curl -s -o /dev/null -w "%{http_code}" "$(cat ./api.txt)/health" | grep -q "200"; 
+do echo "waiting for api to be ready..." && sleep 20; done && echo "API active"
+
+# echo "$(cat ./api.txt)/health"
