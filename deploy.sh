4fea7c3d32b9865ca37c4d774b0756b0d2287e15
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-04-17 04:36:49 +1000

    Removed wait.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ terraform init
 
 terraform apply -auto-approve
 
-until curl -s -o /dev/null -w "%{http_code}" "$(cat ./api.txt)/health" | grep -q "200"; 
-do echo "waiting for api to be ready..." && sleep 20; done && echo "API active"
+#until curl -s -o /dev/null -w "%{http_code}" "$(cat ./api.txt)/health" | grep -q "200"; 
+#do echo "waiting for api to be ready..." && sleep 20; done && echo "API active"
 
 # echo "$(cat ./api.txt)/health"
